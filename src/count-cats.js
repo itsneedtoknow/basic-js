c22678840abc385fabffe23244588276de609b1c
itsneedtoknow <letta_in_love@mail.ru>
2023-04-17 20:10:20 +0300

    feat: add cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,9 +14,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function countCats(matrix) {
+  // throw new NotImplementedError('Not implemented');
+  // let cats = 0;
+  let cats=[];
+  let result;
+  for(let i = 0; i<matrix.length; i++){
+    for(let j = 0; j < matrix[i].length; j++){
+      if(matrix[i][j] === '^^'){
+        cats.push(matrix[i][j])
+        // cats += 1;        
+      }
+    }
+  }
+result = cats.length;
+return result;
 }
 
 module.exports = {
